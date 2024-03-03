6ca0635741e4b70e1218cc04994979eefe1cece6
ndikrp <andhikarizky36@gmail.com>
2024-03-03 22:58:12 +0700

    set up http server, added landing page, added cariMobil navbar, hero, and footer

--- server/index.js
+++ server/index.js
@@ -1,1 +1,20 @@
-console.log("Implement servermu disini yak 😝!");
+const express = require('express');
+const path = require('path');
+const PORT = 3000;
+const app = express();
+
+app.use(express.static(path.join(__dirname, '../public')));
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public', 'index.html'));
+});
+
+app.get('/cars', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public', 'cariMobil.html'));
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Server berjalan di port http://localhost:${PORT}`);
+});
